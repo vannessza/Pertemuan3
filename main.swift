f9dec30df65a99335272f5f070bf49e67e1b8b92
VannessAnthony <>
2022-03-08 01:48:30 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,68 @@
+//  Ternary Operator
+var largestNumber: Int
+
+let number1 = 40
+let number2 = 5
+
+if number1 > number2{
+  largestNumber = number1
+}else{
+  largestNumber = number2
+}
+
+largestNumber = number1 >
+number2 ? number1 : number2
+
+print(largestNumber)
+
+// let letter: Character = "u"
+
+// switch letter{
+// case "a", "i", "u", "e", "o":
+//   print("The letter is a vowel")
+// default:
+//   print("The letter is a consonant")
+// }
+
+// Range operator
+// let distance: Int = 250
+
+// switch distance {
+//   case 0...9:
+//     print("Your destination is          close")
+//   case 10...99:
+//     print("Your destination is not      that for")
+//   default:
+//     print("where are you going")
+// }
+
+
+
+
+// var numberOfwheels = 0
+
+// switch numberOfwheels {
+//   case 0:
+//     print("Missing wheels")
+//   case 1:
+//     print("Unicycle")
+//   case 2:
+//     print("Sicycle")
+//   default:
+//     print("That a lot of wheels")
+// }
+
+// var isRain = true
+
+// if isRain{
+//   print("Bring your umbrella")
+// }
+// var temperature = 100
+
+// if temperature >= 65 && temperature <= 75 {
+//   print("Temperature is okay")
+// }else if temperature < 65 {
+//   print("it is too cold")
+// }else{
+//   print("it is too hot")
+// }
